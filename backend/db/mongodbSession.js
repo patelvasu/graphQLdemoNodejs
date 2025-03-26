d4442225080d3ba219c7b37a6704b353e383cd36
vilson <vilson@aistechnolabs.org>
2025-03-26 07:54:13 +0530

    Update db url

--- backend/db/mongodbSession.js
+++ backend/db/mongodbSession.js
@@ -4,7 +4,8 @@ import connectMongoSession from "connect-mongodb-session";
 
 const mongoDBSession=async()=>{
     return new Promise((resolve, reject) => {
-        const connectionURl=`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+        // const connectionURl=`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+        const connectionURl=process.env.ATLASH_DB_URL;
         const MongoDBStore = connectMongoSession(session);
         const mongoSessionStore = new MongoDBStore({
             uri: connectionURl,
